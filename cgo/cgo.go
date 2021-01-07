19c9f7810024c7896f65914922852c6bebcd0ee1
lreese1 <laurent.reese@free.fr>
2021-01-07 14:49:48 +0100

    added cgo call to manipulate a string (char* in C)

--- cgo/cgo.go
+++ cgo/cgo.go
@@ -3,8 +3,8 @@ package main
 
 import "fmt"
 import "unsafe"
-
 // #include <stdio.h>
+// #include <stdlib.h>
 // // see https://golang.org/cmd/cgo/
 // int fibonacci(int x)
 // {
@@ -12,6 +12,15 @@ import "unsafe"
 //   if (x==0)
 //     return 0;
 //   return (x + fibonacci(x - 1));
+// }
+// char *changeString(char *s)
+// { // "poisson" devient "pCHIENn"
+//	 s[1] = 'C';
+//	 s[2] = 'H';
+//	 s[3] = 'I';
+//	 s[4] = 'E';
+//	 s[5] = 'N';
+//   return s;
 // }
 import "C"
 
@@ -26,6 +35,15 @@ func main() {
 	*z = 12
 	t = int(C.fibonacci(C.int(*z))) // Attention il y a des précautions à prendre car le type int en GO fait 8 octets alors qu'il n'en fait que 4 en C !	
 	fmt.Println(t, unsafe.Sizeof(t), unsafe.Sizeof(m))
+
+
+	var s string = "poisson"
+	fmt.Println("animal=", s)
+	s2 := C.CString(s)
+	defer C.free(unsafe.Pointer(s2)) // ! needs to include stdlib.h otherwise the compiler will say : could not determine kind of name for C.free
+	// prototype C.GoString :  func C.GoString(*C.char) string
+	s3 := C.GoString(C.changeString(s2))
+	fmt.Println("animal=", s3)	
 }
 
 // N.B. voir https://dave.cheney.net/tag/cgo
